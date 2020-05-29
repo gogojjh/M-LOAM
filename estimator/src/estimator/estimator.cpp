6b68aa25de2034b3375917f5923adad44e3fd34f
Linsin <jianglingxin@aliyun.com>
2020-05-29 14:24:10 +0800

    feat: use openmp in inputcloud of estimator and no bug after 10 minutes testing

--- estimator/src/estimator/estimator.cpp
+++ estimator/src/estimator/estimator.cpp
@@ -201,41 +201,62 @@ void Estimator::changeSensorType(int use_imu, int use_stereo)
 
 void Estimator::inputCloud(const double &t, const std::vector<PointCloud> &v_laser_cloud_in)
 {
-    assert(v_laser_cloud_in.size() == NUM_OF_LASER);
+  assert(v_laser_cloud_in.size() == NUM_OF_LASER);
 
-    TicToc measurement_pre_time;
-    std::vector<cloudFeature> feature_frame(NUM_OF_LASER);
+  TicToc measurement_pre_time;
+  std::vector<cloudFeature *> feature_frame_ptr(NUM_OF_LASER);
 
-    for (size_t i = 0; i < v_laser_cloud_in.size(); i++)
+#pragma omp parallel for num_threads(NUM_OF_LASER)
+  for (size_t i = 0; i < v_laser_cloud_in.size(); i++)
+  {
+    float start_ori, end_ori;
+#pragma omp critical
     {
-        float start_ori, end_ori;
-        f_extract_.findStartEndAngle(v_laser_cloud_in[i], start_ori, end_ori);
-        if ((SEGMENT_CLOUD) && (ESTIMATE_EXTRINSIC == 0))
-        {
-            PointCloud laser_cloud_segment;
-            img_segment_.segmentCloud(v_laser_cloud_in[i], laser_cloud_segment);
-            f_extract_.extractCloud(t, laser_cloud_segment, feature_frame[i], start_ori, end_ori);
-        } else
-        {
-            f_extract_.extractCloud(t, v_laser_cloud_in[i], feature_frame[i], start_ori, end_ori);
-        }
+      f_extract_.findStartEndAngle(v_laser_cloud_in[i], start_ori, end_ori);
+    };
+    cloudFeature *tmp_frame(new cloudFeature);
+    if ((SEGMENT_CLOUD) && (ESTIMATE_EXTRINSIC == 0))
+    {
+      PointCloud laser_cloud_segment;
+#pragma omp critical
+      {
+        img_segment_.segmentCloud(v_laser_cloud_in[i], laser_cloud_segment);
+      };
+#pragma omp critical
+      {
+        f_extract_.extractCloud(t, laser_cloud_segment, *tmp_frame, start_ori, end_ori);
+      };
+    } else
+    {
+#pragma omp critical
+      {
+      f_extract_.extractCloud(t, v_laser_cloud_in[i], *tmp_frame, start_ori, end_ori);
+    };
     }
+#pragma omp critical
+  {
+    feature_frame_ptr[i] = tmp_frame;
+  };
+  }
 
-    stringstream ss;
-    for (size_t i = 0; i < feature_frame.size(); i++)
-    {   
-        ss << feature_frame[i]["laser_cloud"].size() << " ";
-        total_corner_feature_ += feature_frame[i]["corner_points_less_sharp"].size();
-        total_surf_feature_ += feature_frame[i]["surf_points_less_flat"].size();
-    }
-    printf("size of after segmentation: %s\n", ss.str().c_str());
-    printf("openmp: meaPre time: %fms (%u*%fms)\n", measurement_pre_time.toc(), v_laser_cloud_in.size(), measurement_pre_time.toc() / v_laser_cloud_in.size());
-    total_measurement_pre_time_ += measurement_pre_time.toc();
+  std::vector<cloudFeature> feature_frame(NUM_OF_LASER);
+  for (size_t i = 0; i < NUM_OF_LASER; i++) feature_frame[i] = *feature_frame_ptr[i];
 
-    m_buf_.lock();
-    feature_buf_.push(make_pair(t, feature_frame));
-    m_buf_.unlock();
-    if (!MULTIPLE_THREAD) processMeasurements();
+  stringstream ss;
+  for (size_t i = 0; i < feature_frame.size(); i++)
+  {
+    ss << feature_frame[i]["laser_cloud"].size() << " ";
+    total_corner_feature_ += feature_frame[i]["corner_points_less_sharp"].size();
+    total_surf_feature_ += feature_frame[i]["surf_points_less_flat"].size();
+  }
+  printf("size of after segmentation: %s\n", ss.str().c_str());
+  printf("openmp: meaPre time: %fms (%u*%fms)\n\n", measurement_pre_time.toc(), v_laser_cloud_in.size(), measurement_pre_time.toc() / v_laser_cloud_in.size());
+  total_measurement_pre_time_ += measurement_pre_time.toc();
+
+  // m_buf_.lock();
+  // feature_buf_.push(make_pair(t, feature_frame));
+  // m_buf_.unlock();
+  // if (!MULTIPLE_THREAD) processMeasurements();
 }
 
 void Estimator::inputCloud(const double &t, const PointCloud &laser_cloud_in)
