b1341a5874d869ad75179455f8b580838a31d8d9
gogojjh <651650302@qq.com>
2020-06-09 12:36:00 +0800

    add g2o && save g2oGraph

--- estimator/test/test_generate_full_map.cpp
+++ estimator/test/test_generate_full_map.cpp
@@ -1,8 +1,25 @@
+/*******************************************************
+ * Copyright (C) 2020, RAM-LAB, Hong Kong University of Science and Technology
+ *
+ * This file is part of M-LOAM (https://ram-lab.com/file/jjiao/m-loam).
+ * If you use this code, please cite the respective publications as
+ * listed on the above websites.
+ *
+ * Licensed under the GNU General Public License v3.0;
+ * you may not use this file except in compliance with the License.
+ *
+ * Author: Jianhao JIAO (jiaojh1994@gmail.com)
+ *******************************************************/
+#include <glog/logging.h>
+#include <gflags/gflags.h>
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <thread>
 #include <mutex>
+#include <queue>
+#include <iomanip>
 
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
@@ -26,10 +43,17 @@
 #include "../src/estimator/pose.h"
 #include "g2oIO/PoseGraphIO.h"
 
-#define LASER_SYNC_THRESHOLD 0.07;
+#define LASER_SYNC_THRESHOLD 0.07
+
+DEFINE_string(save_path, "pose_graph", "path to save the pose graph");
+DEFINE_int32(num_laser, 1, "number of LiDAR for mapping");
+DEFINE_int32(save_interval, 1, "save interval");
+
+std::string path;
+int num_laser;
+int save_interval;
 
 size_t frame_cnt = 0;
-std::string path = "/home/jjiao/trajectory_results/real_vehicle/shandong/rv01_gf/pose_graph/";
 pcl::PCDWriter pcd_writer;
 
 std::queue<sensor_msgs::PointCloud2ConstPtr> cloud_buf;
@@ -37,6 +61,7 @@ std::queue<nav_msgs::OdometryConstPtr> odom_buf;
 std::mutex m_buf;
 
 pcl::PointCloud<pcl::PointXYZI> laser_cloud;
+pcl::PointCloud<pcl::PointXYZI> laser_cloud_map;
 Pose pose_cur_odom;
 
 PoseGraphIO g2osaver;
@@ -100,23 +125,45 @@ void sync_process()
             // laser_cloud_full_map += laser_cloud_filter;
 
             stringstream ss;
-            ss << path << time << ".pcd";
+            ss << std::fixed << std::setprecision(9);
+            ss << path << "pcd/" << time << ".pcd";
             pcd_writer.write(ss.str(), laser_cloud);
             std::cout << "Saving pcd to: " << ss.str() << std::endl;
+
+            if (frame_cnt % save_interval == 0)
+            {
+                for (auto p : laser_cloud.points)
+                    if (int(p.intensity) + 1 <= num_laser)
+                        laser_cloud_map.points.push_back(p);
+            }
             laser_cloud.clear();
 
             g2osaver.insertPose(Eigen::Isometry3d(pose_cur_odom.T_.cast<double>()));
+            std::cout << pose_cur_odom << std::endl;
+
+            frame_cnt++;
         }
     }
 }
 
 int main(int argc, char* argv[])
 {
+    if (argc < 3)
+    {
+        printf("please intput: rosrun mloam test_generate_full_map -help\n");
+        return 1;
+    }
+    google::ParseCommandLineFlags(&argc, &argv, true);
+
     ros::init(argc, argv, "test_generate_full_map");
     ros::NodeHandle nh("~");
 
     ros::Subscriber sub_laser_cloud = nh.subscribe<sensor_msgs::PointCloud2>("/laser_cloud_registered", 5, laserCloudFullResHandler);
     ros::Subscriber sub_laser_map_odometry = nh.subscribe<nav_msgs::Odometry>("/laser_map", 5, laserMapOdometryHandler);
+
+    path = FLAGS_save_path;
+    num_laser = FLAGS_num_laser;
+    save_interval = FLAGS_save_interval;
 
     std::thread sync_thread(sync_process);
     ros::Rate loop_rate(100);
@@ -127,7 +174,13 @@ int main(int argc, char* argv[])
     }
 
     stringstream ss;
-    ss << path << "mloam_map_pose.g2o";
+    ss << path << "mloam_map_pose_" << num_laser << ".g2o";
     std::cout << "Saving g2o to: " << ss.str() << std::endl;
     g2osaver.saveGraph(ss.str());
+
+    laser_cloud_map.width = 1;
+    laser_cloud_map.height = laser_cloud_map.points.size();
+    ss.str("");
+    ss << path << "mloam_cloud_map_" << num_laser << ".pcd";
+    pcd_writer.write(ss.str(), laser_cloud_map);
 }
