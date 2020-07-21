deee28119b3c2b94ebdbaf03a66cc451a640d99c
gogojjh <651650302@qq.com>
2020-07-21 10:43:53 +0800

    modify oxford_batch_testing

--- estimator/script/oxford_bash/test_mloam_realvehicle_oxford_main.sh
+++ estimator/script/oxford_bash/test_mloam_realvehicle_oxford_main.sh
@@ -1,17 +1,23 @@
 # !/bin/bash
 
-export data_path=$DATA_PATH/oxford_radar_dataset/2019-01-18-15-20-12-radar-oxford-10k/data_ds_4900_6500.bag
-export rpg_path=$CATKIN_WS/src/localization/rpg_trajectory_evaluation
-export result_path=$rpg_path/results/real_vehicle/oxford/20190118_4900_6500/
-mkdir -p $result_path/gf_pcd
-
-
 ### ours
 roslaunch mloam mloam_realvehicle_oxford.launch \
     run_mloam:=true \
     result_save:=true \
     gf_method:=gd_float \
     gf_ratio_ini:=0.2 \
+    gnc:=true \
+    loss_mode:=gmc \
+    bag_file:=$data_path \
+    output_path:=$result_path
+sleep 5
+
+### different feature ratio
+roslaunch mloam mloam_realvehicle_oxford.launch \
+    run_mloam:=true \
+    result_save:=true \
+    gf_method:=gd_float \
+    gf_ratio_ini:=1.0 \
     gnc:=true \
     loss_mode:=gmc \
     bag_file:=$data_path \
@@ -64,14 +70,7 @@ roslaunch mloam mloam_realvehicle_oxford.launch \
     output_path:=$result_path
 sleep 5
 
-## different LiDAR SLAM
-roslaunch mloam mloam_realvehicle_oxford.launch \
-    run_mloam:=false \
-    run_legoloam:=true \
-    bag_file:=$data_path \
-    output_path:=$result_path
-sleep 5
-
+### different LiDAR SLAM
 roslaunch mloam mloam_realvehicle_oxford.launch \
     run_mloam:=false \
     run_aloam:=true \
@@ -79,7 +78,16 @@ roslaunch mloam mloam_realvehicle_oxford.launch \
     output_path:=$result_path
 sleep 5
 
+roslaunch mloam mloam_realvehicle_oxford.launch \
+    run_mloam:=false \
+    run_legoloam:=true \
+    bag_file:=$data_path \
+    output_path:=$result_path
+sleep 5
+
+### evaluation
 python2 $rpg_path/scripts/analyze_trajectory_single_mloam.py \
+--recalculate_errors \
 --est_type \
     M-LOAM-gd-float-0.2-gmc-gnc \
     M-LOAM-rnd-0.2-gmc-gnc M-LOAM-fps-0.2-gmc-gnc \
