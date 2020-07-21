edbc2a90c1aa942742702a090e0e8b83aa304ecb
gogojjh <651650302@qq.com>
2020-07-21 10:49:58 +0800

    modify oxford_batch_testing

--- docker/run_mloam.sh
+++ docker/run_mloam.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-roscore &
-ROSCORE_PID=$!
-sleep 1
+# roscore &
+# ROSCORE_PID=$!
+# sleep 1
 
 docker run \
     -it \
@@ -10,10 +10,18 @@ docker run \
     -v ${DATA_PATH}:/usr/app/dataset \
     -p 8050:8888 \
     --name mloam_batch_test \
-    ros:melodic-mloam /bin/bash \
-    "cd /usr/app/catkin_ws/; \
-    catkin config \
-        --cmake-args \
-            -DCMAKE_BUILD_TYPE=Release; \
-        catkin build; \
-        source devel/setup.bash;" 
+    ros:melodic-mloam /bin/bash 
+
+# docker run \
+#     -it \
+#     -v ${CATKIN_WS}/src:/usr/app/catkin_ws/src \
+#     -v ${DATA_PATH}:/usr/app/dataset \
+#     -p 8050:8888 \
+#     --name mloam_batch_test \
+#     ros:melodic-mloam /bin/bash -c \
+#     "cd /usr/app/catkin_ws/; \
+#     catkin config \
+#         --cmake-args \
+#             -DCMAKE_BUILD_TYPE=Release; \
+#         catkin build; \
+#         source devel/setup.bash;" 
