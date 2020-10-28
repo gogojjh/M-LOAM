a8d0844a4387d4bfd5dd6d7508f78d347bebf38b
gogojjh <jiaojh1994@gmail.com>
2020-10-28 21:17:16 +0800

    add bag record in launch

--- estimator/script/rv_oxford_main.sh
+++ estimator/script/rv_oxford_main.sh
@@ -1,16 +1,17 @@
 # !/bin/bash
 
 ################## M-LOAM-GF with different good feature ratio
-# roslaunch mloam mloam_realvehicle_oxford.launch \
-#     run_mloam:=true \
-#     run_mloam_mapping:=true \
-#     with_ua:=true \
-#     gf_method:=wo_gf \
-#     gf_ratio_ini:=1.0 \
-#     result_save:=true \
-#     bag_file:=$data_path \
-#     output_path:=$result_path
-# sleep 5
+roslaunch mloam mloam_realvehicle_oxford.launch \
+    run_mloam:=true \
+    run_mloam_mapping:=true \
+    with_ua:=true \
+    gf_method:=wo_gf \
+    gf_ratio_ini:=1.0 \
+    result_save:=true \
+    bag_file:=$data_path \
+    output_path:=$result_path \
+    bag_record:=true
+sleep 20
 
 # roslaunch mloam mloam_realvehicle_oxford.launch \
 #     run_mloam:=true \
@@ -19,8 +20,9 @@
 #     gf_ratio_ini:=0.8 \
 #     result_save:=true \
 #     bag_file:=$data_path \
-#     output_path:=$result_path
-# sleep 5
+#     output_path:=$result_path \
+#        bag_record:=true
+# sleep 20
 
 # roslaunch mloam mloam_realvehicle_oxford.launch \
 #     run_mloam:=true \
@@ -29,8 +31,9 @@
 #     gf_ratio_ini:=0.6 \
 #     result_save:=true \
 #     bag_file:=$data_path \
-#     output_path:=$result_path
-# sleep 5
+#     output_path:=$result_path \
+#        bag_record:=true
+# sleep 20
 
 # roslaunch mloam mloam_realvehicle_oxford.launch \
 #     run_mloam:=true \
@@ -40,20 +43,22 @@
 #     gf_ratio_ini:=0.4 \
 #     result_save:=true \
 #     bag_file:=$data_path \
-#     output_path:=$result_path
-# sleep 5
+#     output_path:=$result_path \
+#        bag_record:=true
+# sleep 20
 
-roslaunch mloam mloam_realvehicle_oxford.launch \
-    run_mloam:=true \
-    run_mloam_mapping:=true \
-    gf_method:=gd_float \
-    gf_ratio_ini:=0.2 \
-    result_save:=true \
-    bag_file:=$data_path \
-    output_path:=$result_path
-sleep 5
+# roslaunch mloam mloam_realvehicle_oxford.launch \
+#     run_mloam:=true \
+#     run_mloam_mapping:=true \
+#     gf_method:=gd_float \
+#     gf_ratio_ini:=0.2 \
+#     result_save:=true \
+#     bag_file:=$data_path \
+#     output_path:=$result_path \
+#     bag_record:=true
+# sleep 20
 
-################## different point selection methods
+# ################## different point selection methods
 roslaunch mloam mloam_realvehicle_oxford.launch \
     run_mloam:=true \
     run_mloam_mapping:=true \
@@ -62,8 +67,9 @@ roslaunch mloam mloam_realvehicle_oxford.launch \
     gf_ratio_ini:=0.2 \
     result_save:=true \
     bag_file:=$data_path \
-    output_path:=$result_path
-sleep 5
+    output_path:=$result_path \
+    bag_record:=true
+sleep 20
 
 # roslaunch mloam mloam_realvehicle_oxford.launch \
 #     run_mloam:=true \
@@ -72,8 +78,9 @@ sleep 5
 #     gf_ratio_ini:=0.2 \
 #     result_save:=true \
 #     bag_file:=$data_path \
-#     output_path:=$result_path
-# sleep 5
+#     output_path:=$result_path \
+    #    bag_record:=true
+# sleep 20
 
 # roslaunch mloam mloam_realvehicle_oxford.launch \
 #     run_mloam:=true \
@@ -82,28 +89,32 @@ sleep 5
 #     gf_ratio_ini:=0.2 \
 #     result_save:=true \
 #     bag_file:=$data_path \
-#     output_path:=$result_path
-# sleep 5
+#     output_path:=$result_path \
+#     bag_record:=true
+# sleep 20
 
 ################## different LiDAR SLAM
-# roslaunch mloam mloam_realvehicle_oxford.launch \
-#     run_mloam:=false \
-#     run_aloam:=true \
-#     bag_file:=$data_path \
-#     output_path:=$result_path
-# sleep 5
+roslaunch mloam mloam_realvehicle_oxford.launch \
+    run_mloam:=false \
+    run_aloam:=true \
+    bag_file:=$data_path \
+    output_path:=$result_path \
+    bag_record:=true
+sleep 20
 
-# roslaunch mloam mloam_realvehicle_oxford.launch \
-#     run_mloam:=false \
-#     run_floam:=true \
-#     bag_file:=$data_path \
-#     output_path:=$result_path
-# sleep 5
+roslaunch mloam mloam_realvehicle_oxford.launch \
+    run_mloam:=false \
+    run_floam:=true \
+    bag_file:=$data_path \
+    output_path:=$result_path \
+    bag_record:=true
+sleep 20
 
-# roslaunch mloam mloam_realvehicle_oxford.launch \
-#     run_mloam:=false \
-#     run_legoloam:=true \
-#     bag_file:=$data_path \
-#     output_path:=$result_path
-# sleep 5
+roslaunch mloam mloam_realvehicle_oxford.launch \
+    run_mloam:=false \
+    run_legoloam:=true \
+    bag_file:=$data_path \
+    output_path:=$result_path \
+    bag_record:=true
+sleep 20
 
